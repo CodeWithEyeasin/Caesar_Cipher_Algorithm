2d343a7d10a234e4ef96facf44da92f4ca1807ec
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2024-02-05 12:11:53 +0600

    new

--- src/Main.java
+++ src/Main.java
@@ -5,54 +5,60 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the message: ");
-        String originalMessage = scanner.nextLine();
-
-        System.out.print("Enter the shift value: ");
-        int shift = scanner.nextInt();
-
-        System.out.print("Choose an operation (1 for encryption, 2 for decryption): ");
-        int operation = scanner.nextInt();
-
-        String resultMessage;
-
-        switch (operation) {
-            case 1 -> {
-                resultMessage = encrypt(originalMessage, shift);
-                System.out.println("Encrypted message: " + resultMessage);
+        System.out.println("Select One Option:\n1.Message Encryption\n2.Message Decryption");
+        boolean istrue=false;
+        while (!istrue){
+            int Option=scanner.nextInt();
+            switch (Option){
+                case 1: encryption();
+                istrue=true;
+                    break;
+                case 2:decryption();
+                istrue=true;
+                    break;
+                default:
+                    System.out.println("Please Enter The Right Option");
             }
-            case 2 -> {
-                resultMessage = decrypt(originalMessage, shift);
-                System.out.println("Decrypted message: " + resultMessage);
-            }
-            default -> System.out.println("Invalid operation. Please choose 1 for encryption or 2 for decryption.");
         }
 
-        scanner.close();
-    }
+        }
+    private static void encryption() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a message to encrypt: ");
+        String message=scanner.nextLine();
 
-    public static String encrypt(String message, int shift) {
-        StringBuilder result = new StringBuilder();
+        System.out.print("Enter the key value for the message: ");
+
+        int key=scanner.nextInt();
+        message=message.toLowerCase();
         for (int i = 0; i < message.length(); i++) {
-            char character = message.charAt(i);
-            if (Character.isLetter(character)) {
-                char base = Character.isUpperCase(character) ? 'A' : 'a';
-
-                int relativePosition = character - base;
-                int shiftedPosition = relativePosition + shift + 26;
-                int positionShift = shiftedPosition % 26;
-
-                char newCharacter = (char) (base + positionShift);
-
-                result.append(newCharacter);
-            } else {
-                result.append(character);
+            char c=message.charAt(i);
+            if (Character.isLetter(c)){
+                c= (char) ((c-'a' + key + 26) % 26 + 'a');
+                System.out.print(c);
             }
+
         }
-        return result.toString();
+
     }
 
-    public static String decrypt(String encryptedMessage, int shift) {
-        return encrypt(encryptedMessage, -shift);
+    private static void decryption() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a message to decrypt: ");
+        String message=scanner.nextLine();
+
+        System.out.print("Enter the key value for the message: ");
+        int key=scanner.nextInt();
+        message=message.toLowerCase();
+        for (int i = 0; i < message.length(); i++) {
+            char c=message.charAt(i);
+            if (Character.isLetter(c)){
+                c=(char) ((c - 'a' - key + 26) % 26 + 'a');
+                System.out.print(c);
+            }
+
+        }
     }
+
+
 }
